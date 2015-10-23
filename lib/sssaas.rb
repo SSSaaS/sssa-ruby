6dcd71e78a7225e3020608a05ea7897a68c72942
Alexander Scheel <alexander.m.scheel@gmail.com>
2015-10-23 13:20:33 -0500

    Use 256-bit Safe Prime from Go

--- lib/sssaas.rb
+++ lib/sssaas.rb
@@ -9,39 +9,11 @@ module SSSAAS
         #   secret - secret to hide
         def initialize()
             # 512-bit prime
-            @prime = 13405015853489344599299360336760892578089045124351982171123226525026027210088647848509034243564149418363355794565216143100084048507668072099463322790104603
+            @prime = 99995644905598542077721161034987774965417302630805822064337798850767846245779
         end
 
         def random()
             return SecureRandom.random_number(@prime)
-        end
-
-        def inverse(second)
-            a = second
-            b = @prime
-            q = 0
-            r = b
-            lastq = q
-            lastr = r
-
-            while r > 0
-                lastq = q
-                lastr = r
-                q = (a / b).floor
-                r = a - b*q
-                a = b
-                b = r
-            end
-
-            return lastr
-        end
-
-        def create(n, k, secret)
-            return true
-        end
-
-        def use(shares)
-            return true
         end
     end
 end
